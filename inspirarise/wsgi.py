d23fff02526e492c81152cea8e22b2179d4b8d28
madokscharles <charlesmadoks@gmail.com>
2024-08-06 04:12:52 +0100

    Add whitenoise and update wsgi.py

--- inspirarise/wsgi.py
+++ inspirarise/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inspirarise.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
